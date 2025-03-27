f67cc53fcf7cd38a2f1ea563d5df3286d5367543
Ben <150054499+Beleebs@users.noreply.github.com>
2025-03-27 17:27:07 -0400

    Got these jawns set up

---
+++ consumer.cpp
@@ -0,0 +1,15 @@
+// Ben Leber
+// 3/27/25
+// Consumer Program that consumes items in a buffer, should there be any.
+
+#include <iostream>
+#include <thread>
+#include <semaphore>
+
+void consume() {
+
+}
+
+int main() {
+
+}
---
+++ producer.cpp
@@ -0,0 +1,15 @@
+// Ben Leber
+// 3/27/25
+// Producer Program that produces items to be consumed by a consumer.
+
+#include <iostream>
+#include <thread>
+#include <semaphore>
+
+void produce() {
+
+}
+
+int main() {
+
+}
